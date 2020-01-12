6ee1c6ecec101f294df6d4869817246ad0c33d1e
ufoxcode <ufoxcode@gmail.com>
2020-01-13 00:00:09 +0800

    Initial Commit

---
+++ MathQuiz 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MathQuiz 1
+//
+//  Created by UFO Xcode on 2020/1/13.
+//  Copyright © 2020 UFO Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MathQuiz 1Tests/MathQuiz_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MathQuiz_1Tests.swift
+//  MathQuiz 1Tests
+//
+//  Created by UFO Xcode on 2020/1/13.
+//  Copyright © 2020 UFO Xcode. All rights reserved.
+//
+
+import XCTest
+@testable import MathQuiz_1
+
+class MathQuiz_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
